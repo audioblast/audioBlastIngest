9b100a2ab792ef105be2854829c38046c4d323e3
Ed Baker <edwbaker@gmail.com>
2020-11-17 17:47:31 +0000

    fix traitoR.

--- R/traitor.R
+++ R/traitor.R
@@ -1,10 +1,4 @@
-traitoR  <- function(input) {
-  if (is.null(input)) {
-    output <- read.csv("../test_data/traits.txt")
-    #TODO: Provide notice data is out of date
-  } else {
-    #TODO: read from web
-  }
+traitoR  <- function(output) {
   col_names <- c("source", colnames(output))
   output <- cbind(rep_len("bio.acousti.ca", nrow(output)), output)
   colnames(output) <- col_names
