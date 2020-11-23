f473461e0882ff9ae095004977e43b62d94bf3c2
Ed Baker <edwbaker@gmail.com>
2020-11-23 11:10:55 +0000

    get sources from audioBLAST! API

--- R/ingest.R
+++ R/ingest.R
@@ -23,3 +23,22 @@ ingestR <- function(db) {
   uploadTraits(db, traits)
   uploadRecordings(db, recordings)
 }
+
+#' Get sources from audioBLAST! API
+#'
+#' Uses the audioBLAST! API to get a list of data sources
+#'
+#' @export
+#' @importFrom rjson fromJSON
+getSources <- function() {
+  json_data <- fromJSON(file="https://api.audioblast.org/standalone/modules/list_sources/")
+  sources <- list()
+  for (i in 1:length(json_data$data)) {
+    source <- names(json_data$data)[[i]]
+    for (j in 1: length(json_data$data[[1]])) {
+      row <- list(c(source, json_data$data[[i]][[j]]))
+      sources <- c(row, sources)
+    }
+  }
+  return(sources)
+}
