6cd56706b42c437ccf26b83c45893f2a217a5ec9
Ed Baker <edwbaker@gmail.com>
2021-04-25 12:35:52 +0100

    Update uploaders.R

--- R/uploaders.R
+++ R/uploaders.R
@@ -46,6 +46,17 @@ uploadDeployments <- function(db, i) {
   dbWriteTable(db, "deployments", i, overwrite=TRUE)
 }
 
+#' uploadDeploymentLocations
+#'
+#' Replaces the database deployment_locations table with contents of a data frame
+#'
+#' @param db database connector
+#' @param i dataframe of taxa to upload.
+#' @export
+#' @importFrom DBI dbConnect dbWriteTable
+uploadDeploymentLocations <- function(db, i) {
+  dbWriteTable(db, "deployment_locations", i, overwrite=TRUE)
+}
 
 #' uploadDevices
 #'
@@ -57,4 +68,28 @@ uploadDeployments <- function(db, i) {
 #' @importFrom DBI dbConnect dbWriteTable
 uploadDevices <- function(db, i) {
   dbWriteTable(db, "devices", i, overwrite=TRUE)
+}
+
+#' uploadSensors
+#'
+#' Replaces the database sensors table with contents of a data frame
+#'
+#' @param db database connector
+#' @param i dataframe of taxa to upload.
+#' @export
+#' @importFrom DBI dbConnect dbWriteTable
+uploadSensors <- function(db, i) {
+  dbWriteTable(db, "sensors", i, overwrite=TRUE)
+}
+
+#' uploadAbiotic
+#'
+#' Replaces the database abiotic table with contents of a data frame
+#'
+#' @param db database connector
+#' @param i dataframe of taxa to upload.
+#' @export
+#' @importFrom DBI dbConnect dbWriteTable
+uploadAbiotic <- function(db, i) {
+  dbWriteTable(db, "abiotic", i, overwrite=TRUE)
 }
