c8ca651c30dccfa1622fb195d3d2a783a48d12e7
Ed Baker <edwbaker@gmail.com>
2022-02-16 18:29:01 +0000

    Process birdnet vernacular names.

--- R/birdnet-lite.R
+++ R/birdnet-lite.R
@@ -1,7 +1,7 @@
 birdnetVernacular <- function(data) {
   labels <- read.csv('https://raw.githubusercontent.com/kahst/BirdNET-Lite/main/model/labels.txt', sep="_", col.names =c("scientific", "vernacular"), header=F)
   for (i in 1:nrow(data)) {
-    data$taxon[[i]] <-labels$scientific[[which(labels$vernacular==data$taxon[[i]])]]
+    data$taxon[i] <-labels$scientific[[which(labels$vernacular==data$taxon[[i]])]]
   }
   return(data)
 }
