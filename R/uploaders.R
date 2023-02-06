cf254d78c8716efb797ee46eb4868b1c63bc7626
Ed Baker <edwbaker@gmail.com>
2023-02-06 19:06:28 +0000

    Test incremental upload

--- R/uploaders.R
+++ R/uploaders.R
@@ -31,8 +31,46 @@ uploadTraits <- function(db, i) {
 #' @export
 #' @importFrom DBI dbConnect dbWriteTable
 uploadRecordings <- function(db, i) {
-  write.csv(i, file="complete_recordings.csv")
-  dbWriteTable(db, "recordings", i, overwrite=TRUE)
+  #write.csv(i, file="complete_recordings.csv")
+  #dbWriteTable(db, "recordings", i, overwrite=TRUE)
+  fields <- head(table, 1)
+  print(fields)
+  for (i in 1:nrow(table))
+    sql <- paste0("INSERT INTO `recordings` ",
+                  "(`source`, `id`, `Title`, `taxon`, `file`, `author`, ",
+                  "`post_date`, `size`, `size_raw`, `type`, `NonSpecimen`, ",
+                  "`Date`,`Time`,`Duration`) ",
+                  "VALUES (",
+                  "'", table[i,1], "', ",
+                  "'", table[i,2], "', ",
+                  "'", table[i,3], "', ",
+                  "'", table[i,4], "', ",
+                  "'", table[i,5], "', ",
+                  "'", table[i,6], "', ",
+                  "'", table[i,7], "', ",
+                  "'", table[i,8], "', ",
+                  table[i,9], ", ",
+                  "'", table[i,10], "', ",
+                  "'", table[i,11], "', ",
+                  "'", table[i,12], "', ",
+                  "'", table[i,13], "', ",
+                  table[i,14],
+                  ") ON DUPLICATE UPDATE ",
+                  "`Title` = '", table[i,3], "', ",
+                  "`taxon` = '", table[i,4], "', ",
+                  "`file` = '", table[i,5], "', ",
+                  "`author` = '", table[i,6], "', ",
+                  "`post_date`= '", table[i,7], "'",
+                  "`size` = '", table[i,8], "',",
+                  "`size_raw` = ",table[i,9], ", ",
+                  "`type` = '", table[i,10], "', ",
+                  "`NonSpecimen` = '", table[i,11], "', ",
+                  "`Date` = '", table[i,12], "', ",
+                  "`Time` = '", table[i,13], "', ",
+                  "`Duration` = ",table[i,14],
+                  ";"
+    )
+  print(sql);exit;
 }
 
 #' uploadDeployments
@@ -96,6 +134,5 @@ uploadAbiotic <- function(db, i) {
 }
 
 uploadAnnOmate <- function(db, i) {
-  write.csv(i, file="complete_annomate.csv")
   dbWriteTable(db, "annomate", i, overwrite=TRUE)
 }
