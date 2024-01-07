3d56983b56a535ceba6df3e8636a6b4a33dce902
Ed Baker <edwbaker@gmail.com>
2024-01-07 12:50:12 +0000

    Improvements to col

--- R/col.R
+++ R/col.R
@@ -2,37 +2,43 @@
 #'
 #' Flatten the parent-child relationships in the Catalogue of Life taxonomy
 #'
-#' @param data dataframe of the CoL taxonomy
-#' @param matches vector of taxa to match
+#' @param data dataframe of the CoL taxonomy.
+#' @param matches vector of taxa to match.
+#' @param dataPreprocessed Boolean representing whether the data frame has been
+#'   preprocessed by preprocessCoL().
 #' @importFrom stringr str_replace str_trim
 #'
 #' @export
-col2flat  <- function(data, matches) {
-  data <- preprocessCoL(data)
+col2flat  <- function(data, matches, dataPreprocessed=F) {
+  if (!dataPreprocessed) {
+    data <- preprocessCoL(data)
+  }
   ranks_used <- unique(as.character(data$`dwc.taxonRank`))
   ranks_used <- ranks_used[ranks_used != ""]
   col_names <- c("id","taxon","valid", "parent_id", "Rank", ranks_used)
-  num_taxa <- nrow(data)
+  num_taxa <- length(matches)
   output <- data.frame(matrix(NA, nrow=length(matches), ncol=length(col_names)))
   colnames(output) <- col_names
   output$taxon <- matches
 
   for (i in 1:num_taxa) {
-    if (!data[i, "dwc.scientificName"] %in% matches) {
+    print(paste(i, ":", num_taxa))
+    if (!tolower(matches[i]) %in% data[, "lowerName"]) {
       next
     }
-
-    name <- as.character(data[i, "dwc.scientificName"])
-    rank <- as.character(data[i,"dwc.taxonRank"])
+    matched_name <- tolower(as.character(output[i, "taxon"]))
+    use_id <- as.character(data[data$lowerName==matched_name, "dwc.taxonID"])[1]
+    name <- as.character(data[data$`dwc.taxonID`==use_id, "dwc.scientificName"])
+    rank <- as.character(data[data$`dwc.taxonID`==use_id,"dwc.taxonRank"])
     if (rank != "") {
-      output[output$taxon==name,rank] <- as.character(data[i, "dwc.scientificName"])
+      output[i,rank] <- as.character(data[data$`dwc.taxonID`==use_id, "dwc.scientificName"])
     }
-    parent_id <- as.character(data[i, "dwc.parentNameUsageID"])
+    parent_id <- as.character(data[data$`dwc.taxonID`==use_id, "dwc.parentNameUsageID"])
     while (length(parent_id) > 0) {
       parent_rank <- as.character(data[data$`dwc.taxonID`==parent_id, "dwc.taxonRank"])
       if (!identical(parent_rank, character(0))){
         parent_name <- as.character(data[data$`dwc.taxonID`==parent_id, "dwc.scientificName"])
-        output[output$taxon==name,parent_rank] <- parent_name
+        output[i,parent_rank] <- parent_name
       }
       parent_id <- as.character(data[data$`dwc.taxonID`==parent_id, "dwc.parentNameUsageID"])
     }
@@ -42,6 +48,10 @@ col2flat  <- function(data, matches) {
 
 preprocessCoL <- function(data) {
   data$`dwc.scientificName` <- str_trim(str_replace(data$`dwc.scientificName`,paste0("\\Q",data$`dwc.scientificNameAuthorship`,"\\E"), ""))
+  lower <- tolower(data$`dwc.scientificName`)
+  names <- c(colnames(data),"lowerName")
+  data <- cbind(data,lower)
+  colnames(data) <- names
   data$`dwc.taxonomicStatus` <- replace(data$`dwc.taxonomicStatus`, data$`dwc.taxonomicStatus`!="accepted", "not accepted")
   data$`dwc.parentNameUsageID` <- paste0(data$`dwc.parentNameUsageID`, data$`dwc.acceptedNameUsageID`)
   return(data)
