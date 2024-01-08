d38d9742bb32d5c25643f9e84713335071536cf0
Ed Baker <edwbaker@gmail.com>
2024-01-08 10:28:58 +0000

    Better name matching in CoL

--- R/col.R
+++ R/col.R
@@ -6,7 +6,7 @@
 #' @param matches vector of taxa to match.
 #' @param dataPreprocessed Boolean representing whether the data frame has been
 #'   preprocessed by preprocessCoL().
-#' @importFrom stringr str_replace str_trim
+#' @importFrom stringr str_replace str_trim str_to_title
 #'
 #' @export
 col2flat  <- function(data, matches, dataPreprocessed=F) {
@@ -14,7 +14,7 @@ col2flat  <- function(data, matches, dataPreprocessed=F) {
     data <- preprocessCoL(data)
   }
   ranks_used <- unique(as.character(data$`dwc.taxonRank`))
-  ranks_used <- ranks_used[ranks_used != ""]
+  ranks_used <- str_to_title(ranks_used[ranks_used != ""])
   col_names <- c("id","taxon","valid", "parent_id", "Rank", ranks_used)
 
   #Only work with matched names
@@ -25,18 +25,22 @@ col2flat  <- function(data, matches, dataPreprocessed=F) {
   colnames(output) <- col_names
   output$taxon <- matches
 
+
   for (i in seq_along(matches)) {
     #ToDo: Only insert ranks used by audioblast
     matched_name <- tolower(as.character(output[i, "taxon"]))
     use_id <- as.character(data[data$lowerName==matched_name, "dwc.taxonID"])[1]
     name <- as.character(data[data$`dwc.taxonID`==use_id, "dwc.scientificName"])
     rank <- as.character(data[data$`dwc.taxonID`==use_id,"dwc.taxonRank"])
+    output[i, "id"] <- as.character(data[data$`dwc.taxonID`==use_id, "dwc.taxonID"])
     if (rank != "") {
       output[i,rank] <- as.character(data[data$`dwc.taxonID`==use_id, "dwc.scientificName"])
     }
     parent_id <- as.character(data[data$`dwc.taxonID`==use_id, "dwc.parentNameUsageID"])
     while (length(parent_id) > 0) {
+      output[i, "parent_id"] <- parent_id
       parent_rank <- as.character(data[data$`dwc.taxonID`==parent_id, "dwc.taxonRank"])
+      #ToDo: Add parent taxa as new rows
       if (!identical(parent_rank, character(0))){
         parent_name <- as.character(data[data$`dwc.taxonID`==parent_id, "dwc.scientificName"])
         output[i,parent_rank] <- parent_name
